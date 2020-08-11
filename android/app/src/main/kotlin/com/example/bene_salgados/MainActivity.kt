2000f74941ff0decba877fe962af70ad19b688b8
Bruno <ramosrbruno@gmail.com>
2020-08-11 14:44:54 +0100

    Created Pages

---
+++ android/app/src/main/kotlin/com/example/bene_salgados/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bene_salgados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/data/category_data.dart
@@ -0,0 +1,29 @@
+import '../models/category_model.dart';
+
+final categories = [
+  Category(
+    numberOfItens: 43,
+    imagePath: "assets/images/burger.png",
+    categoryName: "Burger",
+  ),
+  Category(
+    numberOfItens: 23,
+    imagePath: "assets/images/pizza.png",
+    categoryName: "Pizza",
+  ),
+  Category(
+    numberOfItens: 23,
+    imagePath: "assets/images/coffee-cup.png",
+    categoryName: "Coffee Cup",
+  ),
+  Category(
+    numberOfItens: 23,
+    imagePath: "assets/images/beer.png",
+    categoryName: "Beer",
+  ),
+  Category(
+    numberOfItens: 23,
+    imagePath: "assets/images/cheeseburger.png",
+    categoryName: "Cheeseburger",
+  ),
+];
